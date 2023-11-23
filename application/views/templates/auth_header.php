75c3cd79b1a1542850fc0547f7aaa29d3edf5ea0
braverious <muhammadraihan877@gmail.com>
2023-11-23 09:11:09 +0700

    Mengedit latar auth_header.php

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -13,13 +13,11 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary" style="background: #1B222C">
+<body style="background-image: url('<?= base_url('assets/img/background/securityGuard-bg3.jpg'); ?>'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
